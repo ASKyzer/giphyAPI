b5c664a0b9725d6131162fa8660854980643580f
Adrian Kyzer <adriankyzer@gmail.com>
2018-02-25 10:15:41 -0500

    queried the url at giphy for the response and set the data array to the var results.  set ratings to the property of the results and appended to the DoM.

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,61 @@ $(document).ready(function(){
 // create some preset buttons that will display at page load
 var cartoonChar = ["bugs bunny", "tweety bird", "mickey mouse"];
 
+// funtion to display the character gifs when the character buttons are pressed.
+function displayCharacterGifs(){
+
+  // var GphApiClient = require('giphy-js-sdk-core')
+  // client = GphApiClient("GhsnVBBwPn1KoNV1IAJLwXdN37LUEw7X");
+
+  /// Gif Search
+  // client.search('gifs', {"q": "cats"})
+  //   .then((response) => {
+  //     response.data.forEach((gifObject) => {
+  //       console.log(gifObject)
+  //     })
+  //   })
+  //   .catch((err) => {
+  //
+  // })
+
+  // takes the data-name attribute of the particular button pressed
+  var cartoonChar = $(this).attr("data-name");
+  // sets variable queryURL to add the data-name (chosen cartoon character).
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cartoonChar + "&api_key=GhsnVBBwPn1KoNV1IAJLwXdN37LUEw7X&limit=15"
+  // use ajox method to query giphy api for the desired cartoon character
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+
+    // since the response we get is an array of data, we set response.data to the variable results
+    var results = response.data;
+    // logs the results
+    console.log(results);
+    // we then loop through the results array to get our properties
+    for (j = 0; j < results.length; j++){
+      //log results[j]
+      console.log(results[j].title);
+    // get the rating for the gif
+    var ratings = $("<p class='rating'>").text("Rating: " + results[j].rating);
+    // log the rating for the results
+    console.log(ratings);
+    // appends the rating to the DOM in the dive cartoonImages
+    $("#cartoonImages").append(ratings)
+
+
+    }
+
+
+
+
+  })
+
+
+
+}
+
+
 // function to render new buttons based on user userInput
 function renderButtons() {
 
@@ -39,7 +94,7 @@ function renderButtons() {
   });
 
 // add event listener for a click on the cartoon character button and display the character gifs.
-// $(document).on("click", ".characterButton", displayCharacterGifs);
+$(document).on("click", ".characterButton", displayCharacterGifs);
 
 // renders the buttons in the DOM
 renderButtons();
